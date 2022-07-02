c58721eac436cc3761e329bc0e5e2b703a65ed5e
Irem Soyaker <irem_95@windowslive.com>
2022-07-03 09:39:30 +1000

    Add project files.

---
+++ Challenges/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenges
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        
+                try
+                {
+                    Console.Write("Please enter a number:");
+                    int num1 = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Please enter another number:");
+                    int num2 = Convert.ToInt32(Console.ReadLine());
+                    int total = num1 + num2;
+                    Console.WriteLine(total);
+                }
+                catch (FormatException a)
+                {
+                    Console.WriteLine(a.Message);
+                }
+             
+  
+                Console.ReadLine();
+            
+           
+
+        }
+    }
+}
